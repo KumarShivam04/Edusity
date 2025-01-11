6d4720ffa736c4e3691d95a4e89b6c2c2e9d2572
KumarShivam04 <kumarshivambgp04@gmail.com>
2025-01-11 12:36:09 +0530

    Initial commit

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import "./Contact.css";
+import msg_icon from "../../assets/msg-icon.png";
+import mail_icon from "../../assets/mail-icon.png";
+import phone_icon from "../../assets/phone-icon.png";
+import location_icon from "../../assets/location-icon.png";
+import white_arrow from "../../assets/white-arrow.png";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Contact = () => {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    // toast.info("Sending your message...", { autoClose: 2000 });
+
+    const formData = new FormData(event.target);
+    formData.append("access_key", "85209dd3-eba7-4155-b28c-de1dc7965936");
+
+    try {
+      const res = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await res.json();
+
+      if (data.success) {
+        toast.success("Form submitted successfully!", { autoClose: 3000 });
+        event.target.reset();
+      } else {
+        toast.error(`Error: ${data.message}`, { autoClose: 3000 });
+      }
+    } catch (error) {
+      toast.error("Failed to submit the form. Please try again.", {
+        autoClose: 3000,
+      });
+    }
+  };
+
+  return (
+    <div className="contact">
+      <div className="contact-col">
+        <h3>
+          Send us a message <img src={msg_icon} alt="" />{" "}
+        </h3>
+        <p>
+          Feel free to reach out through contact form or find our contact
+          information below. Your feedback, questions, and suggestions are
+          important to us as we strive to provide exceptional service to our
+          university community.
+        </p>
+        <ul>
+          <li>
+            <img src={mail_icon} alt="" /> kumarshivambgp04@gmail.com
+          </li>
+          <li>
+            <img src={phone_icon} alt="" /> 8404965015
+          </li>
+          <li>
+            <img src={location_icon} alt="" />
+            560083 kalkere
+            <br />
+            Bangalore, India
+          </li>
+        </ul>
+      </div>
+      <div className="contact-col">
+        <form onSubmit={onSubmit}>
+          <label>Your name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            required
+          />
+          <label>Phone Number</label>
+          <input
+            type="number"
+            name="phone"
+            placeholder="Enter your mobile number"
+            required
+          />
+          <label>Write your messages here</label>
+          <textarea
+            name="message"
+            rows="6"
+            placeholder="Enter your message"
+            required
+          ></textarea>
+          <button type="submit" className="btn dark-btn">
+            Submit now <img src={white_arrow} alt="" />
+          </button>
+        </form>
+      </div>
+  
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Contact;
